a4c9c946f8aa9987e5aa1ded47028b1a82a64579
akshita1204 <akshitatyagi04@gmail.com>
2025-03-08 11:41:34 +0530

    Light Dark Mode

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react"
+
 export default function Header() {
+  const[isdark,setisdark]=useState(false)
+ 
   return (
     <header className="header-container">
       <div className="header-content">
         <h2 className="title">
           <a href="/">Where in the world?</a>
         </h2>
-        <p className="theme-changer" onClick={()=>{document.body.classList.toggle('dark')}}>
-          <i className="fa-regular fa-moon" />
-          &nbsp;&nbsp;Dark Mode
+        <p className="theme-changer" onClick={()=>{document.body.classList.toggle('dark')
+           setisdark(!isdark)
+           }}>
+          <i className={ `fa-solid fa-${isdark ? 'sun' : 'moon'}`} />
+          &nbsp;&nbsp;{isdark? 'Light' : 'Dark'} Mode
         </p>
       </div>
     </header>
